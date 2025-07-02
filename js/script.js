24e0168adcf005fa043ff038d7d66d781bd60649
Arthur Ferreira <tutudacosta996@gmail.com>
2025-07-02 20:10:51 -0300

    etc

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
-let numbers = 0;
 let n1 = undefined;
 let n2 = undefined;
 let res = document.getElementById('res');
 
 function One() {
-    numbers += 1;
-    if (numbers == 1) {
+    numbers1 += 1;
+    if (numbers1 == 1) {
         n1 = 1;
-    } else if (numbers == 2) {
+    } else if (numbers1 == 2) {
         n2 = 1;
     }
     res.innerHTML += '1';
 }
 
+function Two() {
+    numbers2 += 2;
+    if (numbers2 == 2) {
+        n1 = 2;
+    } else if (numbers2 == 4) {
+        n2 = 2;
+    }
+    res.innerHTML += '2';
+}
+
 function Plus() {
     res.innerHTML += '+';
+}
+
+function Calc() {
+    
 }
